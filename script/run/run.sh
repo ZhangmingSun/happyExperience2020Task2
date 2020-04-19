3f5f76489fc22f8c30993b3ea82374c56a9a5b15
ZhangmingSun <zhangming_sun@hotmail.com>
2020-04-19 23:19:42 +0800

    up

--- script/run/run.sh
+++ script/run/run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-#mvn clean package -U
-mvn --version
-#E:/Software/Maven/apache-maven-3.6.3/bin/mvn clean
 
-cd ..; cd ..;
+#mvn --version
+
+: '
+cd ..; cd ..; #必须返回到pom.xml所在的目录，才能进行mvn clean或mvn clean package命令
 mvn clean package
-cd script/data/
+#mvn clean package -U
 if [ $? != 0 ];then
     echo "build failed!"
 	exit 1
 fi
-: '
+
 '
 
 #yarn --cluster zjyprc-hadoop application -kill application_1564666114135_333647
@@ -20,18 +20,23 @@ fi
 
 #scriptPath=script/run #工程相对路径
 #localPath=/home/szm/tencentContest2020/data #本地绝对路径，这个路径不行，会出错!!!
-localPath="E:/Software/Cygwin/setup/home/szm/tencentContest2020/data" #本地绝对路径
+localDataPath="E:/Software/Cygwin/setup/home/szm/tencentContest2020/data" #本地绝对数据保存路径
+localScriptPath="E:/Software/Cygwin/setup/home/szm/tencentContest2020/contest2020V1/script" #本地绝对Script路径
 
-#cat ${localPath}/test | grep hello > ${localPath}/szm.txt
-#echo `cat ${localPath}/szm.txt`
+#cat ${localDataPath}/test | grep hello > ${localDataPath}/szm.txt
+#echo `cat ${localDataPath}/szm.txt`
 
-echo `cat ${localPath}/test | grep hello`
+echo `cat ${localDataPath}/test | grep hello`
 echo "run test.py"
+#/usr/bin/python script/data/test.py
 #E:/Software/Cygwin/setup/usr/bin/python script/data/test.py
 #/cygdrive/e/Software/Cygwin/setup/usr/bin/python script/data/test.py
-#python script/data/test.py #D:\software2\Anaconda3\setup\python.exe
+#python script/data/test.py
+
+#注意python实际的执行目录是: D:\software2\Anaconda3\setup\python.exe，而不是/usr/bin/python
 #python E:/Software/Cygwin/setup/home/szm/tencentContest2020/contest2020V1/script/data/test.py
-/usr/bin/python script/data/test.py
+#python script/data/test.py #这行命令只能在当前script所在的目录才能执行
+python ${localScriptPath}/data/test.py
 
 #=========================XXX========================
 #echo "sh script/feature/gen_group_features.sh 7"
