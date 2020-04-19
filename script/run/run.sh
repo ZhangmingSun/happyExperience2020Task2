69c1117ca4950bee0a809bb1376d4982c176ad64
ZhangmingSun <zhangming_sun@hotmail.com>
2020-04-19 23:21:59 +0800

    up

--- script/run/run.sh
+++ script/run/run.sh
@@ -11,15 +11,10 @@ if [ $? != 0 ];then
     echo "build failed!"
 	exit 1
 fi
-
 '
 
-#yarn --cluster zjyprc-hadoop application -kill application_1564666114135_333647
-#yarn --cluster zjyprc-hadoop application -kill application_1564666114135_334712
-#hadoop --cluster zjyprc-hadoop fs -rm -r ${outputPath}
-
 #scriptPath=script/run #工程相对路径
-#localPath=/home/szm/tencentContest2020/data #本地绝对路径，这个路径不行，会出错!!!
+#localPath=/home/szm/tencentContest2020/data #这个路径相对不行，会出错!!!
 localDataPath="E:/Software/Cygwin/setup/home/szm/tencentContest2020/data" #本地绝对数据保存路径
 localScriptPath="E:/Software/Cygwin/setup/home/szm/tencentContest2020/contest2020V1/script" #本地绝对Script路径
 
@@ -28,24 +23,20 @@ localScriptPath="E:/Software/Cygwin/setup/home/szm/tencentContest2020/contest202
 
 echo `cat ${localDataPath}/test | grep hello`
 echo "run test.py"
+
+#注意python实际的执行目录是: D:\software2\Anaconda3\setup\python.exe，而不是/usr/bin/python
+python ${localScriptPath}/data/test.py
+
+
+
+
+#python E:/Software/Cygwin/setup/home/szm/tencentContest2020/contest2020V1/script/data/test.py
+#python script/data/test.py #这行命令只能在当前script所在的目录才能执行
+#错误命令
 #/usr/bin/python script/data/test.py
 #E:/Software/Cygwin/setup/usr/bin/python script/data/test.py
 #/cygdrive/e/Software/Cygwin/setup/usr/bin/python script/data/test.py
 #python script/data/test.py
-
-#注意python实际的执行目录是: D:\software2\Anaconda3\setup\python.exe，而不是/usr/bin/python
-#python E:/Software/Cygwin/setup/home/szm/tencentContest2020/contest2020V1/script/data/test.py
-#python script/data/test.py #这行命令只能在当前script所在的目录才能执行
-python ${localScriptPath}/data/test.py
-
-#=========================XXX========================
-#echo "sh script/feature/gen_group_features.sh 7"
-#sh script/feature/gen_group_features.sh 7
-
-#echo "run gen_group_features_v2.sh 7 21"
-#python script/tools/MultiAutoRun_8days.py script/feature/gen_group_features_v2.sh 7 21
-
-
 
 echo "Finish the Task!"
 exit 0
--- script/run/run_tmp.sh
+++ script/run/run_tmp.sh
@@ -1,31 +1,42 @@
 #!/bin/bash
 
-#mvn clean package -U
+
+#mvn --version
+
 : '
+cd ..; cd ..; #必须返回到pom.xml所在的目录，才能进行mvn clean或mvn clean package命令
 mvn clean package
+#mvn clean package -U
 if [ $? != 0 ];then
     echo "build failed!"
 	exit 1
 fi
+
 '
 
 #yarn --cluster zjyprc-hadoop application -kill application_1564666114135_333647
 #yarn --cluster zjyprc-hadoop application -kill application_1564666114135_334712
 #hadoop --cluster zjyprc-hadoop fs -rm -r ${outputPath}
 
-scriptPath=script/run #工程相对路径
-#localPath=/home/szm/tencentContest2020/data #本地绝对路径
-localPath="E:/Software/Cygwin/setup/home/szm/tencentContest2020/data" #本地绝对路径
+#scriptPath=script/run #工程相对路径
+#localPath=/home/szm/tencentContest2020/data #本地绝对路径，这个路径不行，会出错!!!
+localDataPath="E:/Software/Cygwin/setup/home/szm/tencentContest2020/data" #本地绝对数据保存路径
+localScriptPath="E:/Software/Cygwin/setup/home/szm/tencentContest2020/contest2020V1/script" #本地绝对Script路径
 
-cat ${localPath}/test | grep szm > ${localPath}/szm.txt
-echo `cat ${localPath}/szm.txt`
+#cat ${localDataPath}/test | grep hello > ${localDataPath}/szm.txt
+#echo `cat ${localDataPath}/szm.txt`
 
-#=========================XXX========================
-#echo "sh script/feature/gen_group_features.sh 7"
-#sh script/feature/gen_group_features.sh 7
+echo `cat ${localDataPath}/test | grep hello`
+echo "run test.py"
+#/usr/bin/python script/data/test.py
+#E:/Software/Cygwin/setup/usr/bin/python script/data/test.py
+#/cygdrive/e/Software/Cygwin/setup/usr/bin/python script/data/test.py
+#python script/data/test.py
 
-#echo "run gen_group_features_v2.sh 7 21"
-#python script/tools/MultiAutoRun_8days.py script/feature/gen_group_features_v2.sh 7 21
+#注意python实际的执行目录是: D:\software2\Anaconda3\setup\python.exe，而不是/usr/bin/python
+#python E:/Software/Cygwin/setup/home/szm/tencentContest2020/contest2020V1/script/data/test.py
+#python script/data/test.py #这行命令只能在当前script所在的目录才能执行
+python ${localScriptPath}/data/test.py
 
 
 
