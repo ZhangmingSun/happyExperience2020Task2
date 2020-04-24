e6055f98a38218944f1a2229b87c39e51b03697a
ZhangmingSun <zhangming_sun@hotmail.com>
2020-04-24 23:27:32 +0800

    update

--- script/data/test.py
+++ script/data/test.py
@@ -3,7 +3,7 @@
 # from inputs import *
 # from metrics import *
 # import os, random
-print('pyhton helloWorld................')
+print('pyhton helloWorld................SZM')
 #print('all_var num:', len(all_var))
 #logger.info('restore the pre-model error.')
 
--- script/run/run_tmp.sh
+++ script/run/run_tmp.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-
 #mvn --version
-
 : '
 cd ..; cd ..; #必须返回到pom.xml所在的目录，才能进行mvn clean或mvn clean package命令
 mvn clean package
@@ -11,12 +9,9 @@ if [ $? != 0 ];then
     echo "build failed!"
 	exit 1
 fi
-
 '
 
-#yarn --cluster zjyprc-hadoop application -kill application_1564666114135_333647
-#yarn --cluster zjyprc-hadoop application -kill application_1564666114135_334712
-#hadoop --cluster zjyprc-hadoop fs -rm -r ${outputPath}
+echo `which python`
 
 #scriptPath=script/run #工程相对路径
 #localPath=/home/szm/tencentContest2020/data #本地绝对路径，这个路径不行，会出错!!!
@@ -37,9 +32,10 @@ localScriptPath="E:/Software/Cygwin/setup/home/szm/KDD_Cup_2020_t2/happyExperien
 
 #注意python实际的执行目录是: D:\software2\Anaconda3\setup\python.exe，而不是/usr/bin/python
 #python E:/Software/Cygwin/setup/home/szm/tencentContest2020/contest2020V1/script/data/test.py
-#python script/data/test.py #这行命令只能在当前script所在的目录才能执行
-python ${localScriptPath}/test/test.py
 
+#python script/data/test.py #这行命令只能在当前script所在的目录才能执行
+#python ${localScriptPath}/test/test.py
+/usr/bin/python ${localScriptPath}/data/test.py
 
 
 echo "Finish the Task!"
